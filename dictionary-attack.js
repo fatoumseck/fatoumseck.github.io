8ccacae10a3fbd7ef3ecfde4497e9ebc4893e060
fatoumseck <fm.seck15@gmail.com>
2017-08-25 10:11:02 -0400

    Add files via upload

---
+++ dictionary-attack.js
@@ -0,0 +1,31 @@
+var wordsList = [];
+
+function init() {
+  // Load the words from the dictionary text file to wordsList
+  var wordsFile = "https://raw.githubusercontent.com/GirlsFirst/SIP-2017/master/Unit2_Applications/dictionary-attack/dictionary.txt?token=ADcVhZjRMd86ZdhPE2jVvIaJdQdzLA6Yks5YvvVSwA%3D%3D";
+  $.get(wordsFile, function(data) {
+    document.getElementById("btnSubmit").disabled = true;
+    wordsList = data.split('\n');
+    document.getElementById("btnSubmit").disabled = false;
+  });
+}
+
+window.onload = init;
+
+/* ADD YOUR CODE BELOW */
+
+function checkPassword() {
+  for (var i = 0; i < wordsList.length; i++) {
+    array[i]
+  }
+  if (i == wordsList) {
+    console.log("Weak password.");
+  }
+    else {
+      console.log("Strong password.");
+    }
+}
+
+if ("pw" == true) {
+  checkPassword();
+}
---
+++ recipe.py
@@ -0,0 +1,31 @@
+ingredients_amount = {
+"UNSALTED BUTTER" : "5 TBSP",
+"ALL PURPOSE FLOUR" : "3 TBSP",
+"WHOLE OR HALF AND HALF MILK" :"2 1/2 CUPS",
+"CHEDDAR CHEESE" : "1 LB",
+"COLBY CHEESE" : "1/2 LB",
+"DIJON MUSTARD" : "1 TBSP",
+"NUTMEG" : "PINCH",
+"CAYENNE PEPPER" : "PINCH",
+"SALT AND PEPPER" : "PINCH",
+"ELBOW MACARONI" : "1 LB",
+"DRY BREAD CRUMBS" : "3/4 CUP"
+}
+
+
+ingredients_instructions = {
+"UNSALTED BUTTER" : "Preheat oven over 350 degrees. Butter a shallow 2-quart baking dish. Melt 3 tapblespoons of the butter in a large saucepan.",
+"ALL PURPOSE FLOUR" : "Add the flour and cook over moderate heat for 2 minutes, stirring constantly.",
+"WHOLE OR HALF AND HALF MILK" : "Add the half-and-half and cook over moderate heat whisking constantly until theckened about 3 minutes.",
+"CHEDDAR CHEESE" : "Add one half of the Cheddar Cheese.",
+"COLBY CHEESE" :  "Add one half of the Colby Cheese and cook over low heat, stirring, until melted.",
+"DIJON MUSTARD" : "Stir.",
+"NUTMEG" : "Stir.",
+"CAYENNE PEPPER" : "Stir.",
+"SALT AND PEPPER" : "Season.",
+"ELBOW MACARONI" : "Cook the elbow macaroni in a large pot of boiling salted water until al dente. Drain very well."
+"CHEESE SAUCE" : "Add the cheese sauce and the remaining cheese and stir until combined.",
+"MACARONI" : "Spread the macaroni in the prepared baking dish.",
+"STEP 2 UNSALTED BUTTER" : "In a small glass bowl, Melt the remaining 2 tablespoons of butter in a microwave oven.",
+"DRY BREAD CRUMBS" : "Add the bread crumbs, season with salt and pepper, and sprinkle the buttered crumbs over the macaroni and bake for 45 minutes, or until bubling and golden on the top. Let stand minutes before serving. Let stand for 15 minutes before serving."
+}
